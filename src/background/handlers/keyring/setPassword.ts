f01dfd63b64cc9346cd0a4f92af9a018e3933405
whitesunset <chester.work@gmail.com>
2020-10-28 20:56:06 +0100

    Refactoring: move password-related errors checking to background

--- src/background/handlers/keyring/setPassword.ts
+++ src/background/handlers/keyring/setPassword.ts
@@ -1,7 +1,26 @@
-import { Letter } from '@/model/Letter'
 import keyring from '@/services/Keyring'
+import { CreatePasswordLetter } from '@/model/Keyring'
+import { ApplicationError, ErrorCode } from '@/model/Error'
+
+const MIN_LENGTH = 8
+
+function passwordsNotEqual (password: string, passwordRepeat: string): boolean {
+  return password !== passwordRepeat
+}
+
+function passwordTooShort (password: string): boolean {
+  return password.length < MIN_LENGTH
+}
 
 // Set Keyring key
-export async function handleSetPassword (message: Letter): Promise<number | undefined> {
-  return keyring.create(message.body)
+export async function handleSetPassword (message: CreatePasswordLetter): Promise<number | undefined> {
+  if (passwordsNotEqual(message.body.password, message.body.passwordRepeat)) {
+    throw new ApplicationError(ErrorCode.PasswordMismatch)
+  }
+
+  if (passwordTooShort(message.body.password)) {
+    throw new ApplicationError(ErrorCode.PasswordShort)
+  }
+
+  return keyring.create(message.body.password)
 }
---
+++ src/model/Keyring.ts
@@ -0,0 +1,11 @@
+import { Letter, LetterSubject } from './Letter';
+
+export type SetPasswordRequest = {
+  password: string;
+  passwordRepeat: string;
+}
+
+export interface CreatePasswordLetter extends Letter {
+  subject: LetterSubject.SetPassword,
+  body: SetPasswordRequest
+}
